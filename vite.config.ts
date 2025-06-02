0f9eb14033ba4288d2b9714abbe3351638650560
AGuwf <kesantiw@gmail.com>
2025-06-03 01:44:54 +0700

    Add gh page for deploy github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-
+  plugins: [react(), tailwindcss()],
+  base: '/react-template/', // This is crucial for GitHub Pages deployment
 })
